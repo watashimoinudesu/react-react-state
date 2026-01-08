976782feaf47dc1704db24ff7dccc100b92e1fd3
KIRATI-SX <Kirati_Srising@outlook.co.th>
2026-01-08 14:28:50 +0700

    feat: implement greeting functionality with dynamic message display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [message,setMessage]=useState('');
+
+  function handleClickGreeting(lang){
+    setMessage(greetMap[lang] || 'other');
+  }
+
+  const greetMap={
+    th:'สวัสดี!',
+    en:'Hi!',
+    cn:'你好!',
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message ? message : 'Greeting Message'}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={()=>handleClickGreeting('th')}>สวัสดี!</button>
+        <button onClick={()=>handleClickGreeting('en')}>Hi!</button>
+        <button onClick={()=>handleClickGreeting('cn')}>你好!</button>
       </div>
     </div>
   );
