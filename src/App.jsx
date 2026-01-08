28c29fc6722e25c8ac7c6d25d997e33d34d2a80d
champ <champdrm@gmail.com>
2026-01-08 13:28:04 +0700

    refactor: use parameterized function for greeting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  const handleGreeting = () => {
+    setGreeting("สวัสดี!");
+  };
+
+  const handleGreeting2 = () => {
+    setGreeting("Hi!");
+  };
+
+  const handleGreeting3 = () => {
+    setGreeting("你好!");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleGreeting}>สวัสดี!</button>
+        <button onClick={handleGreeting2}>Hi!</button>
+        <button onClick={handleGreeting3}>你好!</button>
       </div>
     </div>
   );
